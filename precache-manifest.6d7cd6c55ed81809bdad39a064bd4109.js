fb59bab7b31862d24388f299f0bf1c70d66cba94
ksandr68 <malinsancho@gmail.com>
2020-09-27 15:23:15 +0300

    Updates

---
+++ precache-manifest.6d7cd6c55ed81809bdad39a064bd4109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8567d78616c6dc844aba3221488c1ed",
+    "url": "/trello-app/index.html"
+  },
+  {
+    "revision": "c8d86c8b752049b7fe05",
+    "url": "/trello-app/static/css/main.d3d35adb.chunk.css"
+  },
+  {
+    "revision": "2ed1347c4904e265f8a9",
+    "url": "/trello-app/static/js/2.bdd7ed20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trello-app/static/js/2.bdd7ed20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8d86c8b752049b7fe05",
+    "url": "/trello-app/static/js/main.5cffe3ba.chunk.js"
+  },
+  {
+    "revision": "ea5265e47530f1afe7ed",
+    "url": "/trello-app/static/js/runtime-main.b7109ecb.js"
+  }
+]);
